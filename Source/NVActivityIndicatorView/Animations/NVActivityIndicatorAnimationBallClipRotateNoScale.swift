4594cd36efc982f5474762a164f3a10dd18eb7ba
Victor Sigler <vikt0r.sigler@gmail.com>
2019-05-14 22:58:40 -0400

    Update to Swift 5 and Xcode 10.2.1

--- Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallClipRotateNoScale.swift
+++ Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallClipRotateNoScale.swift
@@ -28,32 +28,32 @@
 import UIKit
 
 class NVActivityIndicatorAnimationBallClipRotateNoScale: NVActivityIndicatorAnimationDelegate {
-    
+
     func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor) {
         let duration: CFTimeInterval = 0.75
-        
+
         // Rotate animation
         let rotateAnimation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
-        
+
         rotateAnimation.keyTimes = [0, 0.5, 1]
         rotateAnimation.values = [0, Double.pi, 2 * Double.pi]
-        
+
         // Animation
         let animation = CAAnimationGroup()
-        
+
         animation.animations = [rotateAnimation]
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-        
+
         // Draw circle
         let circle = NVActivityIndicatorShape.ringThirdFour.layerWith(size: CGSize(width: size.width, height: size.height), color: color)
         let frame = CGRect(x: (layer.bounds.size.width - size.width) / 2,
                            y: (layer.bounds.size.height - size.height) / 2,
                            width: size.width,
                            height: size.height)
-        
+
         (circle as? CAShapeLayer)?.lineWidth = 1.5
         circle.frame = frame
         circle.add(animation, forKey: "animation")
